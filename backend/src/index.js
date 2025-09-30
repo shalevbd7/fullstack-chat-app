42a5145829baf01377047a53539c6305806ebc11
shalev <shalevbd7@gmail.com>
2025-09-30 06:30:11 +0300

    fix: add PORT default and CORS for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ app.use("/api/messages", messageRoute);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/*", (req, res) => {
+  app.get(/^\/(?!api).*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
